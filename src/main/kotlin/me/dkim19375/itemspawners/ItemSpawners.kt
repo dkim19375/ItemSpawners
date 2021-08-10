c20524f43259520c743588bca3650d13dbd77271
dkim19375 <miner8mk@gmail.com>
2021-08-10 08:09:37 -0400

    Add update checker

--- src/main/kotlin/me/dkim19375/itemspawners/ItemSpawners.kt
+++ src/main/kotlin/me/dkim19375/itemspawners/ItemSpawners.kt
@@ -19,6 +19,7 @@
 package me.dkim19375.itemspawners
 
 import io.github.slimjar.app.builder.ApplicationBuilder
+import me.dkim19375.dkimbukkitcore.checker.UpdateChecker
 import me.dkim19375.dkimbukkitcore.config.ConfigFile
 import me.dkim19375.dkimbukkitcore.function.logInfo
 import me.dkim19375.dkimbukkitcore.javaplugin.CoreJavaPlugin
@@ -28,10 +29,12 @@ import me.dkim19375.itemspawners.data.SpawnerData
 import me.dkim19375.itemspawners.manager.SpawnersManager
 import org.bukkit.Bukkit
 import org.bukkit.configuration.serialization.ConfigurationSerialization
+import java.util.logging.Level
 import kotlin.system.measureTimeMillis
 
 class ItemSpawners : CoreJavaPlugin() {
     override val defaultConfig: Boolean = false
+    private val updateChecker = UpdateChecker("95173", null, this)
     val spawnersFile by lazy { ConfigFile(this, "data/spawners.yml") }
     val spawnersManager = SpawnersManager(this)
     var currentTick = 0L
@@ -53,6 +56,7 @@ class ItemSpawners : CoreJavaPlugin() {
         reloadConfig()
         registerCommand("itemspawners", ItemSpawnerCmd(this), ItemSpawnerTab(this))
         Bukkit.getScheduler().runTask(this) {
+            checkForUpdates()
             currentTick++
             Bukkit.getScheduler().runTaskTimer(this, { currentTick++ }, 1L, 1L)
         }
@@ -71,5 +75,21 @@ class ItemSpawners : CoreJavaPlugin() {
     private fun registerConfigs() {
         ConfigurationSerialization.registerClass(SpawnerData::class.java)
         registerConfig(spawnersFile)
+    }
+
+    private fun checkForUpdates() {
+        updateChecker.getSpigotVersion({ version ->
+            if (version == description.version) {
+                logInfo("${description.name} is up to date! ($version)")
+                return@getSpigotVersion
+            }
+            logInfo("${description.name} is outdated!", Level.WARNING)
+            logInfo("Your version: ${description.version}", Level.WARNING)
+            logInfo("Newest version: $version", Level.WARNING)
+            logInfo("Please update here: ${description.website}", Level.WARNING)
+        }) {
+            logInfo("Could not get latest version!", Level.SEVERE)
+            it.printStackTrace()
+        }
     }
 }
